415f76d5c15ccbd11259df47e5f92f135343f253
gololobov <gololobov.serhiy@gmail.com>
2025-04-29 20:15:25 +0300

    29.04.2025

---
+++ Lesson 1.py
@@ -0,0 +1,30 @@
+'''print("Hello Python!")
+print(1234) # це команда, яка виводить на екран інформацію
+print(1234.87987)
+print("Hello", 1234234, 23.457567, True)
+print("Hello", end='\n')
+print("Hello\tPython")
+print("Hello\nPython")
+print("Hello\bPython")'''
+#print("Hello", "Python", 123,  sep=' +=+ ')
+
+# a-z, A-Z, 0-9, _
+# a = 100
+# print(a)
+# c = a
+# a = 999
+# print(a)
+# print(c)
+
+# int
+# float
+# str
+# bool
+
+
+a = int(input("a = "))
+b = int(input("b = "))
+p = 2 * (a + b)
+print("Периметр прямокутника зі сторонами a =", a, ", b =", b, " дорівнює", p)
+
+
